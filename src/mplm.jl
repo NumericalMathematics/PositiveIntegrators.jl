d687dca6cac89a690e588451df3b4c2a0bb0d5f2
Stefan Kopecz <kopecz@mathematik.uni-kassel.de>
2026-02-17 17:28:34 +0100

    avoid nested caches

--- src/mplm.jl
+++ src/mplm.jl
@@ -47,12 +47,7 @@ alg_extrapolates(alg::MPLM33) = true # TODO: Should probably be false
     P3::PType
     d2::dType
     d3::dType
-    α1::T
-    α2::T
-    α3::T
-    β1::T
-    β2::T
-    β3::T
+    αβ::NTuple{6, T}
     step::Int
     small_constant::T2
 end
@@ -78,13 +73,13 @@ function alg_cache(alg::MPLM33, u, rate_prototype, ::Type{uEltypeNoUnits},
     β1 = 9 / 4 * one(uEltypeNoUnits)
     β2 = zero(uEltypeNoUnits)
     β3 = 3 / 4 * one(uEltypeNoUnits)
+    αβ = (α1, α2, α3, β1, β2, β3)
 
     # TODO: This is currently necessary to get the correct type of P (d is of type rateType)
     P, d = evaluate_pds(f, u, p, t)
     # TODO: integrator_stats_nf = 1
 
-    MPLM33oopCache(u, u, P, P, d, d, α1, α2, α3, β1, β2, β3, 1,
-                   alg.small_constant_function(uEltypeNoUnits))
+    MPLM33oopCache(u, u, P, P, d, d, αβ, 1, alg.small_constant_function(uEltypeNoUnits))
 end
 
 struct MPLM43{F, T} <: OrdinaryDiffEqAlgorithm
@@ -95,7 +90,7 @@ end
 alg_order(alg::MPLM43) = 3
 alg_extrapolates(alg::MPLM43) = true # TODO: Should probably be false
 
-@cache mutable struct MPLM43oopCache{uType, PType, dType, T, T2, T3} <:
+@cache mutable struct MPLM43oopCache{uType, PType, dType, T, T2} <:
                       OrdinaryDiffEqConstantCache
     uprevprev::uType
     uprev3::uType
@@ -106,17 +101,9 @@ alg_extrapolates(alg::MPLM43) = true # TODO: Should probably be false
     d2::dType
     d3::dType
     d4::dType
-    α1::T
-    α2::T
-    α3::T
-    α4::T
-    β1::T
-    β2::T
-    β3::T
-    β4::T
+    αβ::NTuple{8, T}
     step::Int
     small_constant::T2
-    MPRK33cache::T3
 end
 
 function MPLM43(; linsolve = LUFactorization(), small_constant = nothing)
@@ -138,6 +125,7 @@ function alg_cache(alg::MPLM43, u, rate_prototype, ::Type{uEltypeNoUnits},
     P, d = evaluate_pds(f, u, p, t)
     # TODO: integrator_stats_nf = 1
 
+    #=
     α1 = zero(uEltypeNoUnits)
     α2 = zero(uEltypeNoUnits)
     α3 = one(uEltypeNoUnits)
@@ -146,6 +134,7 @@ function alg_cache(alg::MPLM43, u, rate_prototype, ::Type{uEltypeNoUnits},
     β3 = 3 / 4 * one(uEltypeNoUnits)
     cache = MPLM33oopCache(u, u, P, P, d, d, α1, α2, α3, β1, β2, β3, 1,
                            alg.small_constant_function(uEltypeNoUnits))
+    =#
 
     α1 = 1 / 4 * one(uEltypeNoUnits)
     α2 = zero(uEltypeNoUnits)
@@ -155,8 +144,69 @@ function alg_cache(alg::MPLM43, u, rate_prototype, ::Type{uEltypeNoUnits},
     β2 = 1 / 3 * one(uEltypeNoUnits)
     β3 = zero(uEltypeNoUnits)
     β4 = 2 / 9 * one(uEltypeNoUnits)
-    MPLM43oopCache(u, u, u, P, P, P, d, d, d, α1, α2, α3, α4, β1, β2, β3, β4, 1,
-                   alg.small_constant_function(uEltypeNoUnits), cache)
+    αβ = (α1, α2, α3, α4, β1, β2, β3, β4)
+    MPLM43oopCache(u, u, u, P, P, P, d, d, d, αβ, 1,
+                   alg.small_constant_function(uEltypeNoUnits))
+end
+
+struct MPLM54{F, T} <: OrdinaryDiffEqAlgorithm
+    linsolve::F
+    small_constant_function::T
+end
+
+alg_order(alg::MPLM54) = 4
+alg_extrapolates(alg::MPLM54) = true # TODO: Should probably be false
+
+@cache mutable struct MPLM54oopCache{uType, PType, dType, T, T2} <:
+                      OrdinaryDiffEqConstantCache
+    uprevprev::uType
+    uprev3::uType
+    uprev4::uType
+    P2::PType
+    P3::PType
+    P4::PType
+    P5::PType
+    d2::dType
+    d3::dType
+    d4::dType
+    d5::dType
+    αβ::NTuple{10, T}
+    step::Int
+    small_constant::T2
+end
+
+function MPLM54(; linsolve = LUFactorization(), small_constant = nothing)
+    if isnothing(small_constant)
+        small_constant_function = floatmin
+    elseif small_constant isa Number
+        small_constant_function = Returns(small_constant)
+    else # assume small_constant isa Function
+        small_constant_function = small_constant
+    end
+    MPLM54(linsolve, small_constant_function)
+end
+
+function alg_cache(alg::MPLM54, u, rate_prototype, ::Type{uEltypeNoUnits},
+                   ::Type{uBottomEltypeNoUnits}, ::Type{tTypeNoUnits}, uprev, uprev2, f, t,
+                   dt, reltol, p, calck,
+                   ::Val{false}) where {uEltypeNoUnits, uBottomEltypeNoUnits, tTypeNoUnits}
+    # TODO: This is currently necessary to get the correct type of P (d is of type rateType)
+    P, d = evaluate_pds(f, u, p, t)
+    # TODO: integrator_stats_nf = 1
+
+    α1 = zero(uEltypeNoUnits)
+    α2 = zero(uEltypeNoUnits)
+    α3 = zero(uEltypeNoUnits)
+    α4 = zero(uEltypeNoUnits)
+    α5 = one(uEltypeNoUnits)
+    β1 = 225 / 96 * one(uEltypeNoUnits)
+    β2 = zero(uEltypeNoUnits)
+    β3 = 50 / 96 * one(uEltypeNoUnits)
+    β4 = 200 / 96 * one(uEltypeNoUnits)
+    β5 = 5 / 96 * one(uEltypeNoUnits)
+    αβ = (α1, α2, α3, α4, α5, β1, β2, β3, β4, β5)
+    MPLM54oopCache(u, u, u, P, P, P, P, d, d, d, d, αβ, 1,
+                   alg.small_constant_function(uEltypeNoUnits))
 end
 
 function initialize!(integrator,
@@ -280,10 +330,15 @@ end
     return v, nfunc, nsolve
 end
 
-@muladd function perform_step_MPLM33_oop(P, d, dt, uprev, uprevprev, uprev3, linsolve,
-                                         cache)
-    (; α1, α2, α3, β1, β2, β3, small_constant) = cache
-    (; P2, P3, d2, d3) = cache
+@muladd function perform_step_MPLM33_oop(P_tup, d_tup, dt, u_tup, linsolve, αβ,
+                                         small_constant)
+    #   (; α1, α2, α3, β1, β2, β3, small_constant) = cache
+    #(; P2, P3, d2, d3) = cache
+
+    P, P2, P3 = P_tup
+    d, d2, d3 = d_tup
+    uprev, uprevprev, uprev3 = u_tup
+    α1, α2, α3, β1, β2, β3 = αβ
 
     # avoid division by zero due to zero Patankar weights
     σ = add_small_constant(uprev, small_constant)
@@ -309,8 +364,7 @@ end
 
 @muladd function perform_step!(integrator, cache::MPLM33oopCache, repeat_step = false)
     (; alg, t, dt, uprev, uprev2, f, p) = integrator
-    #(; uprevprev, uprev3, P2, P3, d2, d3, α1, α2, α3, β1, β2, β3, small_constant) = cache
-    (; uprevprev, uprev3, small_constant) = cache
+    (; uprevprev, uprev3, P2, P3, d2, d3, αβ, small_constant) = cache
 
     #TODO: is this necessary?
     if integrator.u_modified
@@ -359,7 +413,12 @@ end
         P, d = evaluate_pds(f, uprev, p, t)
         integrator.stats.nf += 1
 
-        u = perform_step_MPLM33_oop(P, d, dt, uprev, uprevprev, uprev3, alg.linsolve, cache)
+        P_tup = (P, P2, P3)
+        d_tup = (d, d2, d3)
+        u_tup = (uprev, uprevprev, uprev3)
+
+        u = perform_step_MPLM33_oop(P_tup, d_tup, dt, u_tup, alg.linsolve, αβ,
+                                    small_constant)
         integrator.stats.nsolve += 3
 
         cache.uprev3 = uprevprev
@@ -376,7 +435,7 @@ end
 
 @muladd function perform_substeps_MPLM33_oop(t, dt, num_sub_steps, num_macro_steps, uprev,
                                              uprev2, f, p,
-                                             uprevprev, small_constant, linsolve, cache)
+                                             uprevprev, small_constant, linsolve, αβ)
     nfunc = 0
     nsolve = 0
 
@@ -402,9 +461,8 @@ end
     u = u22[8]
 
     P2, d2 = evaluate_pds(f, uprevprev, p, t)
-    cache.P2 = P2
-    cache.d2 = d2
     nfunc += 1
+
     P, d = evaluate_pds(f, uprev, p, t + dt_sub)
     nfunc += 1
 
@@ -414,15 +472,20 @@ end
         uprevprev = uprev
         uprev = u
 
-        cache.P3 = cache.P2
-        cache.P2 = P
-        cache.d3 = cache.d2
-        cache.d2 = d
+        P3 = P2
+        P2 = P
+        d3 = d2
+        d2 = d
 
         P, d = evaluate_pds(f, uprev, p, t)
         nfunc += 1
 
-        u = perform_step_MPLM33_oop(P, d, dt_sub, uprev, uprevprev, uprev3, linsolve, cache)
+        P_tup = (P, P2, P3)
+        d_tup = (d, d2, d3)
+        u_tup = (uprev, uprevprev, uprev3)
+
+        u = perform_step_MPLM33_oop(P_tup, d_tup, dt_sub, u_tup, linsolve, αβ,
+                                    small_constant)
         push!(v, u)
 
         nsolve += 3
@@ -438,16 +501,20 @@ end
                 uprevprev = uprev
                 uprev = u
 
-                cache.P3 = cache.P2
-                cache.P2 = P
-                cache.d3 = cache.d2
-                cache.d2 = d
+                P3 = P2
+                P2 = P
+                d3 = d2
+                d2 = d
 
                 P, d = evaluate_pds(f, uprev, p, t)
                 nfunc += 1
 
-                u = perform_step_MPLM33_oop(P, d, dt_sub, uprev, uprevprev, uprev3,
-                                            linsolve, cache)
+                P_tup = (P, P2, P3)
+                d_tup = (d, d2, d3)
+                u_tup = (uprev, uprevprev, uprev3)
+
+                u = perform_step_MPLM33_oop(P_tup, d_tup, dt_sub, u_tup, linsolve, αβ,
+                                            small_constant)
                 push!(v, u)
                 nsolve += 3
 
@@ -461,7 +528,8 @@ end
 
 @muladd function perform_step!(integrator, cache::MPLM43oopCache, repeat_step = false)
     (; alg, t, dt, uprev, uprev2, f, p) = integrator
-    (; uprevprev, uprev3, uprev4, P2, P3, P4, d2, d3, d4, α1, α2, α3, α4, β1, β2, β3, β4, small_constant) = cache
+    (; uprevprev, uprev3, uprev4, P2, P3, P4, d2, d3, d4, αβ, small_constant) = cache
+    α1, α2, α3, α4, β1, β2, β3, β4 = αβ
 
     #TODO: is this necessary?
     if integrator.u_modified
@@ -476,10 +544,11 @@ end
         P, d = evaluate_pds(f, uprev, p, t)
         integrator.stats.nf += 1
 
-        # compute starting values using MPLM22 with reduced time step size 
+        # compute starting values using MPLM33 with reduced time step size
+        αβ33 = (0, 0, 1, 9 / 4, 0, 3 / 4)
         v, nf, ns = perform_substeps_MPLM33_oop(t, dt, 4, 3, uprev, uprev2, f, p,
                                                 uprevprev, small_constant, alg.linsolve,
-                                                cache.MPRK33cache)
+                                                αβ33)
         integrator.stats.nf += nf
         integrator.stats.nsolve += ns
 
