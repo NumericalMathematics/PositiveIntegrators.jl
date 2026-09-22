7ec3cdfcf43c5932f171ada74e8107594d6e2367
Stefan Kopecz <kopecz@mathematik.uni-kassel.de>
2026-09-22 20:47:06 +0200

    added prob_pds_jakstat to problem library test

--- test/runtests.jl
+++ test/runtests.jl
@@ -2400,7 +2400,7 @@ end
             probs = (prob_pds_linmod, prob_pds_linmod_inplace, prob_pds_nonlinmod,
                      prob_pds_robertson, prob_pds_bertolazzi, prob_pds_brusselator,
                      prob_pds_npzd,
-                     prob_pds_sir, prob_pds_stratreac)
+                     prob_pds_sir, prob_pds_stratreac, prob_pds_jakstat)
             @testset "$alg" for alg in algs
                 @testset "$i" for (i, prob) in enumerate(probs)
                     if prob == prob_pds_stratreac && alg == SSPMPRK22(0.5, 1.0)
@@ -2428,7 +2428,7 @@ end
             #prob_pds_robertson not included
             probs = (prob_pds_linmod, prob_pds_linmod_inplace, prob_pds_nonlinmod,
                      prob_pds_bertolazzi, prob_pds_brusselator,
-                     prob_pds_npzd, prob_pds_sir, prob_pds_stratreac)
+                     prob_pds_npzd, prob_pds_sir, prob_pds_stratreac, prob_pds_jakstat)
             @testset "$alg" for alg in algs
                 @testset "$prob" for prob in probs
                     tspan = prob.tspan
